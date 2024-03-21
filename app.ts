e66355292fbda6532e0d22a93153f139f6544fb2
Zaid khan <160598429+Zaidkhan009@users.noreply.github.com>
2024-03-22 01:32:28 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,11 @@
+// let users : string [] = ['usama','azeem','bisma','siddique','admin zaid']  
+
+// for (let user of users){
+//     if(user === "admin"){
+//         console.log("Hello admin, Would you like to see a status report?")
+//     }
+
+// else{
+//     console.log(`Hello ${user}, Thank you for logging in again. `)
+// }
+// }
